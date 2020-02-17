c5a0417fe260771172fe79efdbae813dad1fbea2
Junichi TANAKA <junsan45@gmail.com>
2020-02-18 02:07:42 +0900

    Use struct

--- structs-methods-and-interfaces/src/main.rs
+++ structs-methods-and-interfaces/src/main.rs
@@ -1,14 +1,21 @@
-fn perimeter(width: f64, height: f64) -> f64 {
-    return 2.0 * (width + height)
+struct Rectangle {
+    width: f64,
+    height: f64,
 }
 
-fn area(width: f64, height: f64) -> f64 {
-    return width * height
+fn perimeter(rectangle: Rectangle) -> f64 {
+    return 2.0 * (rectangle.width + rectangle.height)
+}
+
+fn area(rectangle: Rectangle) -> f64 {
+    return rectangle.width * rectangle.height
 }
 
 fn main() {
-    println!("{}", perimeter(10.0, 10.0));
-    println!("{}", area(12.0, 6.0));
+    let mut rectangle = Rectangle { width:10.0, height:10.0 };
+    println!("{}", perimeter(rectangle));
+    rectangle = Rectangle { width:12.0, height:6.0 };
+    println!("{}", area(rectangle));
 }
 
 #[cfg(test)]
@@ -17,7 +24,9 @@ mod tests {
 
     #[test]
     fn it_works() {
-        assert_eq!(perimeter(10.0, 10.0), 40.0);
-        assert_eq!(area(12.0, 6.0), 72.0);
+        let mut rectangle = Rectangle { width:10.0, height:10.0 };
+        assert_eq!(perimeter(rectangle), 40.0);
+        rectangle = Rectangle { width:12.0, height:6.0 };
+        assert_eq!(area(rectangle), 72.0);
     }
 }
